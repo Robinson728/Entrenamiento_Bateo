c9ed54957c856a1df2545626fb9c5c901b41fd71
Robinson Manuel Sarante Colon <robinson_sarante@ucne.edu.do>
2021-11-25 16:54:14 -0400

    Creando Model Bateadores.

---
+++ Models/Bateadores.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Entrenamiento_Bateo.Models
+{
+    public class Bateadores
+    {
+        [Key]
+        public int BateadorId { get; set; }
+        public string Nombre { get; set; }
+        public double Porcentaje_Bateo { get; set; }
+        public double Porcentaje_Recta { get; set; }
+        public double Porcentaje_Curva { get; set; }
+        public double Porcentaje_Cambio { get; set; }
+        public double Porcentaje_Slider { get; set; }
+        public double Porcentaje_Nudillo { get; set; }
+    }
+}

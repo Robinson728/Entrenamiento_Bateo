a5fbd4dd6a64e866a1aa02afd8374f6c9eff3150
Robinson Manuel Sarante Colon <robinson_sarante@ucne.edu.do>
2021-11-26 18:38:40 -0400

    Agregando metodos.

--- Models/Bateadores.cs
+++ Models/Bateadores.cs
@@ -11,11 +11,28 @@ namespace Entrenamiento_Bateo.Models
         [Key]
         public int BateadorId { get; set; }
         public string Nombre { get; set; }
-        public double Porcentaje_Bateo { get; set; }
-        public double Porcentaje_Recta { get; set; }
-        public double Porcentaje_Curva { get; set; }
-        public double Porcentaje_Cambio { get; set; }
-        public double Porcentaje_Slider { get; set; }
-        public double Porcentaje_Nudillo { get; set; }
+        public double PorcentajeBateo { get; set; }
+        public double PorcentajeRecta { get; set; }
+        public double PorcentajeCurva { get; set; }
+        public double PorcentajeCambio { get; set; }
+        public double PorcentajeSlider { get; set; }
+        public double PorcentajeNudillo { get; set; }
+        public double PorcentajeHits { get; set; }
+        public double PorcentajeFouls { get; set; }
+        public double BolasBateadas { get; set; }
+        public double RectasLanzadas { get; set; }
+        public double CurvasLanzadas { get; set; }
+        public double SlidersLanzados { get; set; }
+        public double CambiosLanzados { get; set; }
+        public double NudillosLanzados { get; set; }
+        public double Hits { get; set; }
+        public double Fouls { get; set; }
+        public double Afeccion { get; set; }
+        public double RectasBateadas { get; set; }
+        public double CurvasBateadas { get; set; }
+        public double SlidersBateadas { get; set; }
+        public double CambiosBateados { get; set; }
+        public double NudillosBateados { get; set; }
+
     }
 }
